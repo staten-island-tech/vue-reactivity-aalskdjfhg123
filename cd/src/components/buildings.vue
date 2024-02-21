35354f5b7eec9ce1df91cbe36e7669de0c620b42
aalskdjfhg123 <jiangryan8@gmail.com>
2024-02-20 19:43:01 -0500

    FUCK

---
+++ cd/src/components/buildings.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+    <h2>{{ Destinations.name }}</h2>
+    <h2> {{ Destinations.price }}</h2>
+    <button @click="Destinations.effect">upgrade</button>
+    </div>
+</template>
+
+
+
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+    Destinations: Object,
+})
+
+
+</script>
+
+
+
+<style scoped>
+
+</style>
--- cd/src/views/HomeView.vue
+++ cd/src/views/HomeView.vue
@@ -10,35 +10,64 @@
       @click="increment" class="clicker">
       <h1>{{ 1 * clickMulti + clickUpgrade }}</h1>
       <button @click="upgradeClick">upgrade</button>
-    <button>autogen</button>
+    <button @click ="autoUpgrade">autogen</button>
     <button @click="speed">speed</button>
     <h1>{{ tickSpeed }}</h1>
-    <h1>{{ displayCount }}</h1>
-  </div>
+    <h1>{{ count }}</h1>
+    <buildings v-for="things in penis" :key="things.name" :-destinations="things"/>
+
+</div>
 </template>
 
 
 <script setup>
 import { ref } from 'vue'
+import buildings from '@/components/buildings.vue';
+
+
+const upgrades = [
+  {
+    name: "upgrade1",
+    price: 10,
+   /*  effect: function(){
+      upgradeClick()
+    } */
+  },
+  {
+    name: "upgrade2",
+    price: 20
+  },
+  {
+    name: "upgrade3",
+    price: 30
+  },
+  {
+    name: "upgrade4",
+    price: 40
+  },
+] 
+console.log(upgrades)
 
 const count = ref(0)
-const clickUpgrade = ref(0)
-const clickMulti = ref(1)
-const autoCount = ref(1)
-const displayCount = count
+ const clickUpgrade = ref(0)
+ const clickMulti = ref(1)
+const autoCount = ref(0)
 const tickSpeed = ref(1000)
 
-function upgradeClick() {
+ function upgradeClick() {
   clickUpgrade.value++
-}
+} 
 
 function multiClick() {
   clickMulti.value *= 2
 }
 
+function autoUpgrade(){
+  autoCount.value++
+}
+
 function autoIncrement() {
   count.value += autoCount.value
-  console.log(tickSpeed)
 }
 function speed() {
   tickSpeed.value *= .5;
@@ -53,7 +82,39 @@ function increment() {
 }
 
 
+
+const penis = [
+        {
+          name: "Venetian Renaissance",
+          price: 1300,
+          effect: function(){
+            upgradeClick()
+          }
+        },
+        {
+          name: "Swiss Mountain Getaway",
+          price: 800,
+        },
+        {
+          name: "Ibizan Coastal Vacation",
+          price: 1200,
+        },
+        {
+          name: "Hawaiian Vacation",
+          price: 900,
+        },
+        {
+          name: "Cascade Mountains",
+          price: 400,
+        },
+        {
+          name: "Egyptian Exploration",
+          price: 800,
+        },
+      ]
 </script>
+
+
 
 
 
