d7c5322ac50ef80ff6018f197c11b96468161990
aalskdjfhg123 <jiangryan8@gmail.com>
2024-02-21 21:17:27 -0500

    clickerclickerclickerclickerclickerclickerclickerclickerclicker

--- cd/src/components/buildings.vue
+++ cd/src/components/buildings.vue
@@ -2,6 +2,7 @@
     <div>
     <h2>{{ Destinations.name }}</h2>
     <h2> {{ Destinations.price }}</h2>
+    <h3>{{ Destinations.power.value }}</h3>
     <button @click="Destinations.effect">upgrade</button>
     </div>
 </template>
@@ -13,7 +14,8 @@ import { ref } from 'vue';
 
 const props = defineProps({
     Destinations: Object,
-})
+}) //if i change it from destinations to something else the code get very angry
+
 
 
 </script>
--- cd/src/views/HomeView.vue
+++ cd/src/views/HomeView.vue
@@ -2,21 +2,20 @@
   <div>
     <button @click="upgradeClick"> upgrade</button>
     <h1>{{ clickUpgrade }}</h1>
-
+    <!-- 
     <button @click="multiClick"> mult</button>
-    <h1>{{ clickMulti }}</h1>
+    <h1>{{ clickMulti }}</h1> -->
 
     <img src="https://th.bing.com/th/id/R.ee9ad5f805f79dd1a32db3221b4ea5ed?rik=DzBLh1BSTRI35Q&pid=ImgRaw&r=0" alt=""
       @click="increment" class="clicker">
-      <h1>{{ 1 * clickMulti + clickUpgrade }}</h1>
-      <button @click="upgradeClick">upgrade</button>
-    <button @click ="autoUpgrade">autogen</button>
-    <button @click="speed">speed</button>
+    <h1>{{ 1 /* * clickMulti */ + clickUpgrade }}</h1>
+    <button @click="upgradeClick">upgrade</button>
+    <button @click="tick">speed</button>
     <h1>{{ tickSpeed }}</h1>
     <h1>{{ count }}</h1>
-    <buildings v-for="things in penis" :key="things.name" :-destinations="things"/>
+    <buildings v-for="things in upgrades" :key="things.name" :-destinations="things" />
 
-</div>
+  </div>
 </template>
 
 
@@ -24,94 +23,79 @@
 import { ref } from 'vue'
 import buildings from '@/components/buildings.vue';
 
+let upgrades = {
+  upgrade1: {
+    price: 100,
+    power: ref(1),
+    effect: function () {
+      this.power.value++
+    }
+  },
+  upgrade2: {
+    price: 200,
+    power: ref(0),
+    effect: function () {
+      this.power.value++
+    }
+  },
+  upgrade3: {
+    price: 300,
+    power: ref(0),
+    effect: function () {
+      this.power.value++
+    }
+  },
+  upgrade4: {
+    price: 400,
+    power: ref(0),
+    effect: function () {
+      this.power.value++
+    }
+  },
+}
 
-const upgrades = [
-  {
-    name: "upgrade1",
-    price: 10,
-   /*  effect: function(){
-      upgradeClick()
-    } */
-  },
-  {
-    name: "upgrade2",
-    price: 20
-  },
-  {
-    name: "upgrade3",
-    price: 30
-  },
-  {
-    name: "upgrade4",
-    price: 40
-  },
-] 
-console.log(upgrades)
 
 const count = ref(0)
- const clickUpgrade = ref(0)
- const clickMulti = ref(1)
-const autoCount = ref(0)
+const clickUpgrade = ref(0)
+/* const clickMulti = ref(1)
+ */
 const tickSpeed = ref(1000)
 
- function upgradeClick() {
-  clickUpgrade.value++
-} 
 
-function multiClick() {
+function makeGen(maker, makee) {
+  makee.power.value += maker.power.value
+}
+
+function upgradeClick() {
+  clickUpgrade.value++
+}
+
+/* function multiClick() {
   clickMulti.value *= 2
 }
-
-function autoUpgrade(){
-  autoCount.value++
-}
+ */
 
 function autoIncrement() {
-  count.value += autoCount.value
+  count.value += upgrades.upgrade1.power.value
 }
-function speed() {
-  tickSpeed.value *= .5;
-  clearInterval(interval);
-  interval = setInterval(autoIncrement, tickSpeed.value);
+function tick() {
+  tickSpeed.value *= .95;
+  clearInterval(interval, interval);//removes old tickspeed so the new and old dont stack
+  interval = setInterval(autoIncrement, tickSpeed.value);//new tickspeed
+  interval1 = setInterval(makeGen, tickSpeed.value, upgrades.upgrade2, upgrades.upgrade1);
+  interval2 = setInterval(makeGen, tickSpeed.value, upgrades.upgrade3, upgrades.upgrade2);
+  interval3 = setInterval(makeGen, tickSpeed.value, upgrades.upgrade4, upgrades.upgrade3);
 }
-
 let interval = setInterval(autoIncrement, tickSpeed.value);
+let interval1 = setInterval(makeGen, tickSpeed.value, upgrades.upgrade2, upgrades.upgrade1);
+let interval2 = setInterval(makeGen, tickSpeed.value, upgrades.upgrade3, upgrades.upgrade2);
+let interval3 = setInterval(makeGen, tickSpeed.value, upgrades.upgrade4, upgrades.upgrade3);
+//there is 1000% a better way to do this, i am just not smart enough to knpw
 
 function increment() {
   count.value += 1 * clickMulti.value + clickUpgrade.value
 }
 
-
-
-const penis = [
-        {
-          name: "Venetian Renaissance",
-          price: 1300,
-          effect: function(){
-            upgradeClick()
-          }
-        },
-        {
-          name: "Swiss Mountain Getaway",
-          price: 800,
-        },
-        {
-          name: "Ibizan Coastal Vacation",
-          price: 1200,
-        },
-        {
-          name: "Hawaiian Vacation",
-          price: 900,
-        },
-        {
-          name: "Cascade Mountains",
-          price: 400,
-        },
-        {
-          name: "Egyptian Exploration",
-          price: 800,
-        },
-      ]
 </script>
 
 
